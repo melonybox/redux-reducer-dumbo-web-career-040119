0a88080a598b5b3c9839a3db38d83de1309cf29f
melonybox <grandmadcow@gmail.com>
2019-06-20 10:25:53 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,25 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action) {
+  switch(action.type) {
+    case "ADD_FRIEND":
+      return (
+        {...state,
+            friends: [
+              ...state.friends,
+              action.friend
+            ]
+        }
+      )
+      case "REMOVE_FRIEND":
+        const indexPosition = state.friends.findIndex(friend => friend.id === action.id)
+        return (
+          {...state,
+              friends: [
+                ...state.friends.slice(0, indexPosition),
+                ...state.friends.slice(indexPosition + 1)
+              ]
+          }
+        )
+    default:
+      return state;
+  }
 }
