f0f6ca8899d6119898dafa70485b5269b4d0a54b
Gino Desangles <blushingbox@yahoo.com>
2019-06-20 14:16:46 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {
+  friends: [],
+}, action) {
+  switch(action.type) {
+
+    case "ADD_FRIEND":
+      return (
+        {...state,
+            friends: [
+              ...state.friends,
+              action.friend
+            ]
+        }
+    default:
+      return state;
+  }
 }
